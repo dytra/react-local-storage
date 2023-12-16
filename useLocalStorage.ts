ad032f61fe0d68ac887057e5e21e7ddb53ba09ed
dytra <vancielnugraha@gmail.com>
2023-12-16 23:29:29 +0700

    re-enable initialValue and add jsDoc

--- useLocalStorage.ts
+++ useLocalStorage.ts
@@ -1,14 +1,21 @@
 import { useState, Dispatch, SetStateAction, useEffect } from "react";
 
 type SetValue<T> = Dispatch<SetStateAction<T>>;
-
+/**
+ * Custom hook for managing local storage and state.
+ *
+ * @template T - The type/interface of the value stored in local storage.
+ * @param {string} key - The key used to store and retrieve the value from local storage.
+ * @param {T} [initialValue] - The initial value to use if the key is not found in local storage.
+ * @returns {[T, SetValue<T>, () => void]} - An array containing the current value, a function to update the value, and a function to clear the value from the local storage.
+ */
 const useLocalStorage = <T>(
-  key: string
-  // initialValue: T
+  key: string,
+  initialValue?: T
 ): [T, SetValue<T>, () => void] => {
   // Read from localStorage
   const storedValue = localStorage.getItem(key);
-  const initial = storedValue ? storedValue : null;
+  const initial = storedValue ? storedValue : initialValue ? initialValue : null;
 
   // State to hold the current value
   const [value, setValue] = useState<T>(initial as T);
@@ -42,7 +49,10 @@ const useLocalStorage = <T>(
   }, []);
 
   useEffect(() => {
-  },[storedValue]);
+    if(!storedValue && initial) {
+      updateValue(initial as T);
+    }
+  },[storedValue,initial]);
 
   return [value, updateValue, deleteValue];
 };
